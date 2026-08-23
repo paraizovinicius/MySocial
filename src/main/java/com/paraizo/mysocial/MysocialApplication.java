2e2833137527a2a1e3bd0b752863deace824abff
paraizovinicius <paraizovinicius@gmail.com>
2026-08-23 13:58:08 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/paraizo/mysocial/MysocialApplication.java
@@ -0,0 +1,13 @@
+package com.paraizo.mysocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paraizo/mysocial/MysocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paraizo.mysocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
